fa37a9b9de1abf087b748aab4941fbd95f5777e5
Dmitry Afonasenko <dmitrytbp@gmail.com>
2026-06-08 10:38:30 +0500

    feat: Добавил WidgetRegistry

---
+++ src/registry/widgetRegistry.ts
@@ -0,0 +1,37 @@
+import type { WidgetId, WidgetManifest } from "@/types/widget.types";
+import type { Component } from "vue";
+
+export class WidgetRegistry {
+  private widgets = new Map<
+    WidgetId,
+    { manifest: WidgetManifest; component: Component }
+  >();
+
+  register<TManifest extends WidgetManifest>(
+    manifest: TManifest,
+    component: Component,
+  ): void {
+    if (this.widgets.has(manifest.id)) {
+      throw new Error(
+        `Виджет с ID {manifest.id} уже зарегистрирован в системе`,
+      );
+    }
+    this.widgets.set(manifest.id, { manifest, component });
+  }
+
+  getManifest(id: WidgetId): WidgetManifest | undefined {
+    return this.widgets.get(id)?.manifest;
+  }
+
+  getComponent(id: WidgetId): Component | undefined {
+    return this.widgets.get(id)?.component;
+  }
+
+  has(id: WidgetId): boolean {
+    return this.widgets.has(id);
+  }
+
+  getAll(): Array<{ manifest: WidgetManifest; component: Component } | []> {
+    return Array.from(this.widgets.values());
+  }
+}
