6ecd328c9489f7bc9cffe786fa65765512acb06b
Dmitry Afonasenko <dmitrytbp@gmail.com>
2026-06-08 11:12:32 +0500

    chore: добавил alias + отформатировал

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,22 @@
-import { fileURLToPath } from "node:url";
-import { mergeConfig, defineConfig, configDefaults } from "vitest/config";
-import tailwindcss from "@tailwindcss/vite";
-import viteConfig from "./vite.config";
+import { fileURLToPath } from 'node:url';
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import viteConfig from './vite.config';
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
     test: {
-      environment: "jsdom",
-      exclude: [...configDefaults.exclude, "e2e/**"],
-      root: fileURLToPath(new URL("./", import.meta.url)),
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
     },
   }),
 );
